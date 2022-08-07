8031cf9ed47a96a1f99afd722fca12f713074432
Aramuni <39601135+Aramunii@users.noreply.github.com>
2022-08-07 11:48:08 -0300

    init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 app.use(
     cors({
@@ -10,8 +12,29 @@ app.use(
 );
 app.options('*', cors());
 
-app.get('/', (req, res) => res.send('Working!!!'));
+app.get('/', async (req, res) => {
+    var response = await axios.get('https://pt.wikipedia.org/wiki/J%C3%B4_Soares');
+    var $ = cheerio.load(response.data);
+    var Links = $('#content').find('a').toArray().map(element => {
+        var href = $(element).attr('href');
+        if (!$(element).hasClass('external') && href && href.indexOf('/wiki') == 0) {
+            var title = $(element).text();
+            if (title) {
+                return { title: title, link: href };
+            }
+        }
+    })
 
-app.listen(process.env.PORT || 3000, function() {
+    Links = Links.filter(element=>{
+        if(element)
+        {
+            return element;
+        }
+    });
+
+    res.send(Links)
+});
+
+app.listen(process.env.PORT || 3000, function () {
     console.log('server running on port 3000', '');
 });
